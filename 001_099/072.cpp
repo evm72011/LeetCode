f02e044a4ded8a5ab2a6d01f87e4b87cfa63b671
Eugen Michaels <evm72011@gmail.com>
2026-05-03 12:48:22 +0200

    Normalize line endings

--- 001_099/072.cpp
+++ 001_099/072.cpp
@@ -7,7 +7,12 @@ using namespace std;
 class Solution {
 public:
     int minDistance(string word1, string word2) {
+        size_t size = word2.size();
+        vector<int> dp(size + 1, 0);
 
+        for (size_t i = 0; i < size; ++i) {
+
+        }
     }
 };
 
