423ace5fe816c1bb9224ea587d7c39ee25eab3a6
Eugen Michaels <evm72011@gmail.com>
2026-05-03 12:22:08 +0200

    72 blank

---
+++ 001_099/072.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+
+    }
+};
+
+int main() {
+    string word1 {"horse"}, word2 {"ros"};
+    Solution sol;
+    cout << sol.minDistance(word1, word2) << '\n';
+}
